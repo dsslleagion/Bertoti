b714fa216ae03cd7c44f957d2ff37ab7e8d8af10
dsslleagion <dionisio.leao@fatec.sp.gov.br>
2023-02-27 09:40:15 -0300

    pattern

---
+++ Engenharia-3/Strategy Pattern 1.0/Strategy Pattern/Strategy Pattern/motor1_0.java
@@ -0,0 +1,5 @@
+public class motor1_0 implements aceleracao{
+    public void acelera(){
+        System.out.println("Acelera-se o motor com 1000 cavalos.");
+    }
+}
---
+++ Engenharia-3/Strategy Pattern 1.0/Strategy Pattern/Strategy Pattern/motor1_5.java
@@ -0,0 +1,5 @@
+public class motor1_5 implements aceleracao{
+    public void acelera(){
+        System.out.println("Acelera-se o motor com 1500 cavalos.");
+    }
+}
